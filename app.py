a51e0028b2279b556d565c78e35c85c62c286952
bishih <45815232+bishih@users.noreply.github.com>
2021-05-04 14:52:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app.config.update({
         title='current account Api',
         version='v1',
         plugins=[MarshmallowPlugin()],
-        openapi_version='2.0.0'
+        openapi_version='2.0'
     ),
     'APISPEC_SWAGGER_URL': '/swagger/',  # URI to access API Doc JSON
     'APISPEC_SWAGGER_UI_URL': '/swagger-ui/'  # URI to access UI of API Doc
